f7b44fd6491f64d4f2019ba71669d9c56672d92a
Ashma-Garg <ashmagarg77u@gmail.com>
2024-03-16 16:15:45 -0400

    sign out bug resolved

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,18 +1,18 @@
 import { About } from "../modules/about";
 import { DigiCareDrawer } from "../modules/homepage/drawer/Drawer";
-// import { useContext, useEffect } from "react";
-// import { AppContext } from "../context/app";
-// import { ContextProps } from "../context/interface";
-// import { routesName } from "../router/RoutesList";
+import { useContext, useEffect } from "react";
+import { AppContext } from "../context/app";
+import { ContextProps } from "../context/interface";
+import { routesName } from "../router/RoutesList";
 
 export const DigicareAbout = () => {
-  // const { isSignedIn, navigationAsPerSignedStatus } = useContext(
-  //   AppContext
-  // ) as ContextProps;
+  const { isSignedIn, navigationAsPerSignedStatus } = useContext(
+    AppContext
+  ) as ContextProps;
 
-  // useEffect(() => {
-  //   navigationAsPerSignedStatus(routesName.about);
-  // }, [isSignedIn]);
+  useEffect(() => {
+    navigationAsPerSignedStatus(routesName.about);
+  }, [isSignedIn]);
 
   return (
     <DigiCareDrawer>
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,11 +1,21 @@
 
 import { DigiCareDrawer } from "../modules/homepage/drawer/Drawer";
 import { DigiProfile } from "../modules/profile/Profile";
-
+import { AppContext } from "../context/app";
+import { ContextProps } from "../context/interface";
+import { routesName } from "../router/RoutesList";
+import { useEffect, useContext } from "react";
 
 
 export const DigicareProfile = () => {
-
+    const { isSignedIn, navigationAsPerSignedStatus } = useContext(
+        AppContext
+      ) as ContextProps;
+    
+      useEffect(() => {
+        navigationAsPerSignedStatus(routesName.profile);
+      }, [isSignedIn]);
+    
     return (
         <DigiCareDrawer>
             <DigiProfile />
